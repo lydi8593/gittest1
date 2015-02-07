8032a25cc5140853f09bf3b613f9d23cbd6a8377
lydi <lydi8593@gmail.com>
2015-02-07 12:43:51 +0530

    first time commit

---
+++ simplejar/src/simplejar/hello.java
@@ -0,0 +1,14 @@
+package simplejar;
+
+public class hello {
+
+	public static void displayhello()
+	{
+		System.out.println("display hello");
+	}
+
+public static void main(String args[])
+{
+	
+}
+}
