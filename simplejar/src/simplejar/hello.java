efbc89843db99f86c5a0c389e524288e61b253f2
lydi <lydi8593@gmail.com>
2015-02-07 13:10:17 +0530

    kk

--- simplejar/src/simplejar/hello.java
+++ simplejar/src/simplejar/hello.java
@@ -7,6 +7,7 @@ public class hello {
 		System.out.println("display hello");
 		System.out.println("display hello2");
 		
+		System.out.println("display hefffllo2");
 	}
 
 public static void main(String args[])
