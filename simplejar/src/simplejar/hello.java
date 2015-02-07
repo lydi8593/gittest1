6263e1e5072b26867e308521eaa046d4fcae389f
lydi <lydi8593@gmail.com>
2015-02-07 12:49:03 +0530

    second time commit

--- simplejar/src/simplejar/hello.java
+++ simplejar/src/simplejar/hello.java
@@ -5,6 +5,8 @@ public class hello {
 	public static void displayhello()
 	{
 		System.out.println("display hello");
+		System.out.println("display hello2");
+		
 	}
 
 public static void main(String args[])
